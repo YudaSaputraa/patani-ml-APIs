ba00b47b29a8edbb128e96527bce20421d60f0d4
Yuda Saputra <yudasaputra082@gmail.com>
2023-12-21 02:51:10 +0700

    initial commit

---
+++ ml.js
@@ -0,0 +1,453 @@
+
+// Import necessary libraries
+import express from 'express';
+import fetch from 'node-fetch';
+
+// Initialize Express app
+const app = express();
+app.use(express.json());
+const get_predictions = async (inputModel, komoditi) => {
+    const url = `https://patani-ml-5yrauczmnq-et.a.run.app/v1/models/patani-${komoditi}:predict`;
+    const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            instances: inputModel,
+            // [
+            //   [
+            //     [0.72413793],
+            //     [0.86206897],
+            //     [0.86206897],
+            //     [0.86206897],
+            //     [0.86206897],
+            //     [0.86206897],
+            //     [0.86206897],
+            //     [0.86206897],
+            //   ],
+            // ],
+        }),
+    });
+    const respJson = await response.json();
+    return respJson["predictions"];
+    // console.log(respJson.predictions[0][0]);
+};
+
+// get_forecast();
+let komoditi = 'bayam';
+const LAST_DATE_IN_DATA = "2023-11-24";
+let newKomoditi = komoditi.toUpperCase();
+let init_value_forecasts;
+let min_series;// Ganti dengan nilai yang sesuai
+let max_series;
+if (newKomoditi === 'BAYAM') {
+    init_value_forecasts = [
+        [[12000], [14000], [14000], [14000], [14000], [14000], [14000], [14000]],
+    ];
+    min_series = 1500;
+    max_series = 14500;
+} else if (newKomoditi === 'KANGKUNG') {
+    init_value_forecasts = [
+        [[13500], [13500], [13500], [13500], [13500], [13500], [13500], [13500]],
+    ];
+    min_series = 1000;
+    max_series = 13500;
+} else if (newKomoditi === 'SAWI') {
+    init_value_forecasts = [
+        [[6000], [7500], [7500], [7500], [7500], [7500], [7500], [7500]],
+    ];
+    min_series = 3000;
+    max_series = 20000;
+} else if (newKomoditi === 'BROKOLI') {
+    init_value_forecasts = [
+        [[35500], [35500], [35500], [35500], [35500], [35500], [35500], [35500]],
+    ];
+    min_series = 9000;
+    max_series = 38000;
+} else if (newKomoditi === 'KUBIS') {
+    init_value_forecasts = [
+        [[14000], [14000], [14000], [14000], [14000], [14000], [14000], [14000]],
+    ];
+    min_series = 3000;
+    max_series = 15000;
+} else if (newKomoditi === 'WORTEL') {
+    init_value_forecasts = [
+        [[16000], [16000], [16000], [16000], [16000], [16000], [16000], [16000]],
+    ];
+    min_series = 8000;
+    max_series = 18000;
+} else if (newKomoditi === 'KENTANG') {
+    init_value_forecasts = [
+        [[15000], [15000], [15000], [15000], [15000], [15000], [15000], [15000]],
+    ];
+    min_series = 6000;
+    max_series = 20000;
+} else if (newKomoditi === 'KACANG POLONG') {
+    init_value_forecasts = [
+        [[58500], [58500], [58500], [58500], [58500], [58500], [58500], [58500]],
+    ];
+    min_series = 30000;
+    max_series = 60000;
+} else if (newKomoditi === 'KACANG TANAH') {
+    init_value_forecasts = [
+        [[30000], [30000], [30000], [30000], [30000], [30000], [30000], [30000]],
+    ];
+    min_series = 24000;
+    max_series = 13500;
+} else if (newKomoditi === 'BUNCIS') {
+    init_value_forecasts = [
+        [[33500], [33500], [33500], [33500], [33500], [33500], [33500], [33500]],
+    ];
+    min_series = 15000;
+    max_series = 35000;
+} else if (newKomoditi === 'LABU SIAM') {
+    init_value_forecasts = [
+        [[20500], [20500], [20500], [20500], [18000], [18000], [18000], [18000]],
+    ];
+    min_series = 16000;
+    max_series = 21000;
+} else if (newKomoditi === 'LABU KUNING') {
+    init_value_forecasts = [
+        [[10000], [10000], [11500], [11500], [11500], [11500], [11500], [11500]],
+    ];
+    min_series = 10000;
+    max_series = 13500;
+} else if (newKomoditi === 'LOBAK MERAH') {
+    init_value_forecasts = [
+        [[36000], [36000], [36000], [36000], [36000], [36000], [36000], [36000]],
+    ];
+    min_series = 28000;
+    max_series = 40000;
+} else if (newKomoditi === 'LOBAK PUTIH') {
+    init_value_forecasts = [
+        [[31000], [30000], [30000], [30000], [30000], [30000], [30000], [30000]],
+    ];
+    min_series = 27500;
+    max_series = 35000;
+} else if (newKomoditi === 'TOMAT') {
+    init_value_forecasts = [
+        [[12000], [15500], [15500], [15500], [15500], [15500], [15500], [15500]],
+    ];
+    min_series = 5000;
+    max_series = 17000;
+} else if (newKomoditi === 'TERONG') {
+    init_value_forecasts = [
+        [[12500], [12500], [13000], [13000], [13000], [13000], [13000], [13000]],
+    ];
+    min_series = 1000;
+    max_series = 13500;
+} else if (newKomoditi === 'PAPRIKA HIJAU') {
+    init_value_forecasts = [
+        [[37000], [37000], [37000], [37000], [37000], [37000], [37000], [37000]],
+    ];
+    min_series = 36000;
+    max_series = 41500;
+} else if (newKomoditi === 'PAPRIKA KUNING') {
+    init_value_forecasts = [
+        [[80000], [80000], [80000], [80000], [80000], [80000], [80000], [80000]],
+    ];
+    min_series = 75000;
+    max_series = 80000;
+} else if (newKomoditi === 'PAPRIKA MERAH') {
+    init_value_forecasts = [
+        [[79000], [79000], [79000], [79000], [79000], [79000], [79000], [79000]],
+    ];
+    min_series = 60000;
+    max_series = 80000;
+} else if (newKomoditi === 'BAWANG BOMBAY') {
+    init_value_forecasts = [
+        [[20000], [20000], [20000], [20000], [20000], [20000], [20000], [20000]],
+    ];
+    min_series = 19000;
+    max_series = 26500;
+} else if (newKomoditi === 'BAWANG PUTIH') {
+    init_value_forecasts = [
+        [[34000], [34000], [34000], [34000], [34000], [36000], [36000], [36000]],
+    ];
+    min_series = 25000;
+    max_series = 36000;
+} else if (newKomoditi === 'BAWANG MERAH') {
+    init_value_forecasts = [
+        [[30000], [30000], [32000], [32000], [32000], [32000], [32000], [32000]],
+    ];
+    min_series = 22000;
+    max_series = 35000;
+} else if (newKomoditi === 'CABAI KERITING MERAH') {
+    init_value_forecasts = [
+        [[34500], [36000], [36000], [36000], [36000], [36000], [36000], [36000]],
+    ];
+    min_series = 20000;
+    max_series = 45000;
+} else if (newKomoditi === 'CABAI KERITING HIJAU') {
+    init_value_forecasts = [
+        [[34000], [34000], [34000], [34000], [34000], [38000], [38000], [38000]],
+    ];
+    min_series = 22000;
+    max_series = 38000;
+} else if (newKomoditi === 'CABAI RAWIT HIJAU') {
+    init_value_forecasts = [
+        [[29000], [29000], [29500], [29500], [29500], [29500], [29500], [29500]],
+    ];
+    min_series = 15000;
+    max_series = 33000;
+} else if (newKomoditi === 'CABAI RAWIT MERAH') {
+    init_value_forecasts = [
+        [[78000], [78000], [78000], [78000], [78000], [78000], [78000], [78000]],
+    ];
+    min_series = 30000;
+    max_series = 85000;
+} else if (newKomoditi === 'CABAI RAWIT KERITING') {
+    init_value_forecasts = [
+        [[69000], [69000], [69000], [69000], [69000], [69000], [69000], [69000]],
+    ];
+    min_series = 28500;
+    max_series = 70500;
+} else if (newKomoditi === 'MENTIMUN') {
+    init_value_forecasts = [
+        [[30000], [35000], [35000], [35000], [35000], [35000], [35000], [35000]],
+    ];
+    min_series = 30000;
+    max_series = 35000;
+} else if (newKomoditi === 'PARE PARIA') {
+    init_value_forecasts = [
+        [[20000], [20000], [20000], [20000], [20000], [20000], [20000], [20000]],
+    ];
+    min_series = 16000;
+    max_series = 21500;
+} else if (newKomoditi === 'JAMUR') {
+    init_value_forecasts = [
+        [[152000], [152000], [152000], [152000], [152000], [152000], [152000], [152000]],
+    ];
+    min_series = 35000;
+    max_series = 160000;
+} else if (newKomoditi === 'KEMBANG KOL') {
+    init_value_forecasts = [
+        [[18000], [17000], [17000], [17000], [17000], [17000], [17000], [17000]],
+    ];
+    min_series = 14000;
+    max_series = 27500;
+} else if (newKomoditi === 'DAUN BAWANG') {
+    init_value_forecasts = [
+        [[31000], [31000], [31000], [31000], [31000], [31000], [31000], [31000]],
+    ];
+    min_series = 18000;
+    max_series = 42500;
+} else if (newKomoditi === 'KEMANGI') {
+    init_value_forecasts = [
+        [[44500], [44500], [44500], [44500], [44500], [44500], [44500], [44500]],
+    ];
+    min_series = 27500;
+    max_series = 44500;
+} else if (newKomoditi === 'SELADA') {
+    init_value_forecasts = [
+        [[5000], [5000], [5000], [5000], [5000], [5000], [5000], [5000]],
+    ];
+    min_series = 5000;
+    max_series = 9000;
+} else if (newKomoditi === 'SELEDRI') {
+    init_value_forecasts = [
+        [[11500], [11500], [13000], [13000], [13000], [13000], [13000], [13000]],
+    ];
+    min_series = 9000;
+    max_series = 20000;
+} else if (newKomoditi === 'BENIH KACANG PANJANG') {
+    init_value_forecasts = [
+        [[21000], [21000], [21000], [21000], [21000], [21000], [21000], [21000]],
+    ];
+    min_series = 15000;
+    max_series = 25000;
+} else if (newKomoditi === 'BENIH KANGKUNG') {
+    init_value_forecasts = [
+        [[46500], [46500], [46500], [46500], [46500], [46500], [46500], [46500]],
+    ];
+    min_series = 34000;
+    max_series = 50000;
+} else if (newKomoditi === 'BENIH JAGUNG MANIS') {
+    init_value_forecasts = [
+        [[14000], [14000], [14000], [14000], [14000], [14000], [14000], [14000]],
+    ];
+    min_series = 3000;
+    max_series = 14000;
+} else if (newKomoditi === 'BENIH TIMUN') {
+    init_value_forecasts = [
+        [[13000], [13000], [13000], [13000], [13000], [13000], [13000], [13000]],
+    ];
+    min_series = 6000;
+    max_series = 13000;
+} else if (newKomoditi === 'BENIH BUNCIS') {
+    init_value_forecasts = [
+        [[10000], [10000], [10000], [10000], [10000], [10000], [10000], [10000]],
+    ];
+    min_series = 5000;
+    max_series = 12000;
+} else if (newKomoditi === 'CANGKUL SAWAH') {
+    init_value_forecasts = [
+        [[35000], [35000], [35000], [35000], [35000], [35000], [35000], [35000]],
+    ];
+    min_series = 22000;
+    max_series = 35000;
+} else if (newKomoditi === 'BAJAK SINGKAL') {
+    init_value_forecasts = [
+        [[72500], [72500], [72500], [72500], [72500], [72500], [72500], [72500]],
+    ];
+    min_series = 30000;
+    max_series = 75000;
+} else if (newKomoditi === 'GARU SISIR') {
+    init_value_forecasts = [
+        [[810000], [810000], [810000], [810000], [810000], [810000], [810000], [830000]],
+    ];
+    min_series = 550000;
+    max_series = 850000;
+} else if (newKomoditi === 'GARU PIRING') {
+    init_value_forecasts = [
+        [[9000], [9000], [9000], [9000], [9000], [9000], [9000], [9000]],
+    ];
+    min_series = 5000;
+    max_series = 9000;
+} else if (newKomoditi === 'LADA') {
+    init_value_forecasts = [
+        [[149000], [149000], [149000], [149000], [149000], [149000], [149000], [149000]],
+    ];
+    min_series = 89000;
+    max_series = 149000;
+} else if (newKomoditi === 'CENGKEH') {
+    init_value_forecasts = [
+        [[178000], [199000], [199000], [199000], [199000], [199000], [199000], [199000]],
+    ];
+    min_series = 85000;
+    max_series = 210000;
+} else if (newKomoditi === 'KAYU MANIS') {
+    init_value_forecasts = [
+        [[22500], [22500], [22500], [22500], [22500], [22500], [22500], [22500]],
+    ];
+    min_series = 15000;
+    max_series = 25000;
+} else if (newKomoditi === 'KAPULAGA') {
+    init_value_forecasts = [
+        [[87000], [87000], [87000], [87000], [87000], [87000], [87000], [87000]],
+    ];
+    min_series = 82000;
+    max_series = 116000;
+} else if (newKomoditi === 'KUNYIT') {
+    init_value_forecasts = [
+        [[9000], [9000], [9000], [9000], [9000], [9000], [9000], [9000]],
+    ];
+    min_series = 3500;
+    max_series = 10000;
+} else if (newKomoditi === 'UBI JALAR') {
+    init_value_forecasts = [
+        [[9500], [9500], [9500], [9500], [9500], [9500], [9500], [9500]],
+    ];
+    min_series = 3000;
+    max_series = 10000;
+} else if (newKomoditi === 'UBI KAYU') {
+    init_value_forecasts = [
+        [[6000], [6000], [6000], [6000], [6000], [6000], [6000], [6000]],
+    ];
+    min_series = 2500;
+    max_series = 6500;
+} else if (newKomoditi === 'SINGKONG') {
+    init_value_forecasts = [
+        [[2000], [2000], [2000], [2000], [2000], [2000], [2000], [2000]],
+    ];
+    min_series = 1000;
+    max_series = 3000;
+} else if (newKomoditi === 'TALAS') {
+    init_value_forecasts = [
+        [[24500], [24500], [24500], [24500], [24500], [24500], [24500], [24500]],
+    ];
+    min_series = 12000;
+    max_series = 24500;
+} else {
+    // ngarang
+    init_value_forecasts = [
+        [[13000], [24500], [20000], [15000], [13000], [13000], [13000], [13000]],
+    ];
+    min_series = 12000;
+    max_series = 30000;
+} // Ganti dengan nilai yang sesuai
+const norm_init_value_forecasts = [
+    init_value_forecasts[0].map((val) => [(val[0] - min_series) / max_series]),
+];
+
+const get_forecast = async (start_date_target, end_date_target, komoditi) => {
+    const last_date = new Date(LAST_DATE_IN_DATA);
+    const start_date = new Date(start_date_target);
+    const end_date = new Date(end_date_target);
+
+    const number_week_start = Math.floor(
+        (start_date - last_date) / (7 * 24 * 3600 * 1000)
+    ); // per week
+    const number_week_end = Math.floor(
+        (end_date - last_date) / (7 * 24 * 3600 * 1000)
+    ); // per week
+
+    const dict_of_forecasts = {};
+    let init_value_forecasts = norm_init_value_forecasts;
+
+    let result_forecast;
+    for (let num_week = 0; num_week <= number_week_end; num_week++) {
+        // start with 0 (current week)
+        if (num_week === 0) {
+            // Still using last data (since same week - 28/7/2023)
+            result_forecast = init_value_forecasts[init_value_forecasts.length - 1];
+        } else {
+            // Lakukan prediksi menggunakan model (model.predict())
+            // result_forecast = model.predict(init_value_forecasts)[0][-1][0]; // request ke API TF Serving
+            // Perbarui init_value_forecasts
+
+            result_forecast = await get_predictions(init_value_forecasts, komoditi);
+            result_forecast = result_forecast[0][result_forecast.length - 1][0];
+            init_value_forecasts[0].shift();
+            let temp_init_value_forecast = init_value_forecasts;
+            temp_init_value_forecast[0].push([result_forecast]);
+            init_value_forecasts = temp_init_value_forecast;
+            console.log("🔥 input", init_value_forecasts);
+        }
+
+        if (number_week_start <= num_week && num_week <= number_week_end) {
+            const init_date_in_seconds =
+                last_date.getTime() + num_week * (7 * 24 * 3600 * 1000);
+            for (let num_day = 0; num_day < 7; num_day++) {
+                const target_date_in_seconds =
+                    init_date_in_seconds + num_day * (24 * 3600 * 1000);
+                if (
+                    start_date.getTime() <= target_date_in_seconds &&
+                    target_date_in_seconds <= end_date.getTime()
+                ) {
+                    dict_of_forecasts[new Date(target_date_in_seconds).toISOString()] =
+                        result_forecast * max_series + min_series;
+                }
+            }
+        }
+    }
+
+    return dict_of_forecasts;
+};
+
+
+app.post('/inputPrediction', async (req, res) => {
+    try {
+        const { startDate, endDate, komoditi } = req.body;
+        get_forecast(startDate, endDate, komoditi).then((result) =>
+            res.status(200).json({
+                status: 'success',
+                message: 'successfully input prediction',
+                result
+            }));
+        // get_forecast(startDate, endDate, komoditi).then((result) =>
+        //     console.log(result)
+        // );
+    } catch (error) {
+        res.status(500).json({
+            status: 'error',
+            message: 'An error occurred',
+            error: error.message
+        });
+    }
+});
+
+// Start the server on a specified port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
